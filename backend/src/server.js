069d2d1d54028bc8a770e012e396b452d4e4e3ae
szwitt <szwitt.1337@gmail.com>
2024-05-10 13:46:57 +0200

    Deleted server.js comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,15 +14,8 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
   
-// tu pozniej to przeslac do bazy danych
 app.post("/api/lobby", (req,res) => {
     console.log(req.body.lobbyName);
     console.log(req.body.isPrivate);
     console.log(req.body.lobbyPass);
 }); 
-
-/*
-// to musze zmienic aby ten startpage sie odpalal pierwszy
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../.././frontend/views/lobby/lobby.html'));
-}); */
