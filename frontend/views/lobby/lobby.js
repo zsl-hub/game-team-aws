6934734b2a30ad2fe1a0d1bd48f2e3af8ecd5d32
kamilp3szk0 <96954869+kamilp3szk0@users.noreply.github.com>
2024-05-15 11:31:04 +0200

    #48 add nickname input (#49)
    * #48 added nickname input
    
    * #48 fixed Kamil code so it cooperates with vite
    
    * #48 remove my changes made by mistake
    
    * #48 deleted old lobby.css
    
    ---------
    
    Co-authored-by: yez69 <tymekmarek2@gmail.com>

--- frontend/views/lobby/lobby.js
+++ frontend/views/lobby/lobby.js
@@ -1,104 +1,85 @@
 import config from "../../config/config.json";
 
+assignOpenJoinModalListeners();
+document.getElementById("open-modal").addEventListener("click", () => { openModal(); });
+document.getElementById("close-modal").addEventListener("click", () => { closeModal(); });
+document.getElementById("join-lobby").addEventListener("click", () => { joinLobby(); });
+document.getElementById("modal-button-cancel").addEventListener("click", () => { CancelLobby(); });
+document.getElementById("create-lobby").addEventListener("click", () => { createLobby(); });
+
+function assignOpenJoinModalListeners()
+{
+  const elements = document.getElementsByClassName("open-join-modal");
+  for(let i = 0; i < elements.length; i++){
+    elements[i].addEventListener("click", () => { openJoinModal(); });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  checkOverflow();
 
-  window.addEventListener('resize', function() {
-      checkOverflow();
-  });
-
-  // Add event listener for the switch toggle
   var switchInput = document.querySelector('.switch input');
   switchInput.addEventListener('change', togglePasswordInput);
 
-  // Change icon based on switch state
   switchInput.addEventListener('change', function() {
       var iconSpan = document.querySelector('.material-symbols-outlined');
       if (this.checked) {
           iconSpan.textContent = 'lock';
       } else {
-          iconSpan.textContent = 'lock_open'; // unlock icon
+          iconSpan.textContent = 'lock_open';
       }
   });
 });
 
-function checkOverflow() {
-  var lobby = document.querySelector('.lobby');
-
-  if (lobby.scrollHeight > lobby.clientHeight) {
-      lobby.style.overflowY = 'scroll';
-  } else {
-      lobby.style.overflowY = 'hidden';
-  }
-}
-
-document.getElementById("openmodal").addEventListener("click", () => { openModal(); });
-document.getElementsByClassName("modalButton")[0].addEventListener("click", () => { createLobby() });
-document.getElementsByClassName("modalButton")[1].addEventListener("click", () => { closeModal() });
-
 function openModal() {
-  var modal = document.getElementById("modalID");
+  var modal = document.getElementById("ModalID");
   modal.style.display = "block";
 }
 
-/* Idk why it is here ~Szymon  
 function openJoinModal() {
   var joinModal = document.getElementById("JoinModal");
   joinModal.style.display = "block";
-} */
+}
 
 function closeModal() {
-  var modal = document.getElementById("modalID");
+  var modal = document.getElementById("ModalID");
   modal.style.display = "none";
 }
 
 function createLobby() {
-  var lobbyName = document.getElementById("text").value;
-  var switchInput = document.querySelector('.switch input');
-  var passwordInput = document.getElementById("passwordInput");
-  var switchChecked = switchInput.checked;
+    var lobbyName = document.getElementById("text").value;
+    var switchInput = document.querySelector('.switch input');
+    var passwordInput = document.getElementById("password-input");
+    var switchChecked = switchInput.checked;
 
-  if (switchChecked) {
-      var password = passwordInput.value;
-      if (password.length < 4) {
-          alert("Password must consist of at least 4 letters!");
-          return;
-      }
-  }
+    if (switchChecked) {
+        var password = passwordInput.value;
+        if (password.length < 4) {
+            alert("Hasło musi mieć co najmniej 4 znaki!");
+            return;
+        }
+    }
 
-  var newLobbyContainer = document.createElement("div");
-  newLobbyContainer.className = "room";
+    var lobbyTemplate = document.getElementById("lobbyTemplate");
+    var newLobbyContainer = lobbyTemplate.cloneNode(true);
+    newLobbyContainer.removeAttribute("id");
+    newLobbyContainer.style.display = "block";
 
-  var newLobbyLink = document.createElement("a");
-  newLobbyLink.className = "lobbyLink";
-  newLobbyLink.href = "#";
-  newLobbyLink.textContent = lobbyName;
+    var lobbyNameSpan = newLobbyContainer.querySelector(".text123");
+    lobbyNameSpan.textContent = lobbyName;
 
-  var joinButton = document.createElement("button");
-  joinButton.className = "joinLobbyButton";
-  joinButton.textContent = "JOIN";
-  joinButton.onclick = openJoinModal;
+    var iconSpan = newLobbyContainer.querySelector(".material-symbols-outlined");
+    iconSpan.textContent = switchChecked ? "lock" : "lock_open";
 
-  var iconSpan = document.createElement("span");
-  iconSpan.className = "material-symbols-outlined";
-  if (switchChecked) {
-      iconSpan.textContent = "lock";
-  } else {
-      iconSpan.textContent = "lock_open"; 
-  }
+    var lobbyContainer = document.querySelector(".lobby");
+    lobbyContainer.appendChild(newLobbyContainer);
 
-  newLobbyContainer.appendChild(newLobbyLink);
-  newLobbyContainer.appendChild(joinButton);
-  newLobbyContainer.appendChild(iconSpan);
+    assignOpenJoinModalListeners();
 
-  var lobbyContainer = document.querySelector(".lobby");
-  lobbyContainer.appendChild(newLobbyContainer);
-
-  closeModal();
+    closeModal();
 }
 
 function togglePasswordInput() {
-  var passwordInputContainer = document.querySelector('.passwordContainer');
+  var passwordInputContainer = document.querySelector('.password-input-container');
   var switchChecked = this.checked;
 
   if (switchChecked) {
@@ -116,47 +97,99 @@ function joinLobby() {
   joinModal.style.display = "none";
 }
 
-function openJoinModal() {
-  var joinModal = document.getElementById("JoinModal");
-  var switchInput = document.querySelector('.switch input');
-  var passwordInputContainer = document.querySelector('.password-input-container');
+// function openJoinModal() {
+//   var joinModal = document.getElementById("JoinModal");
+//   var switchInput = document.querySelector('.switch input');
+//   var passwordInputContainer = document.querySelector('.password-input-container');
 
-  var iconSpan = document.querySelector('.material-symbols-outlined');
-  var iconType = iconSpan.textContent;
+//   var iconSpan = document.querySelector('.material-symbols-outlined');
+//   var iconType = iconSpan.textContent;
 
-  if (iconType === 'lock') {
-      passwordInputContainer.style.display = "block";
-  } else {
-      passwordInputContainer.style.display = "none";
-  }
-  joinModal.style.display = "block";
-}
+//   if (iconType === 'lock') {
+//       passwordInputContainer.style.display = "block";
+//   } else {
+//       passwordInputContainer.style.display = "none";
+//   }
+  
+//   joinModal.style.display = "block";
+// }
 
+let x1=0, y1=0;
+window.client
+const 
+  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0),
+  dist_to_draw = 50,
+  delay = 1000,
+  fsize = [
+    '1.1rem', '1.4rem', '.8rem', '1.7rem'
+  ],
+  colors = [
+  '#E23636',
+  '#F9F3EE',
+  '#E1F8DC',
+  '#B8AFE6',
+  '#AEE1CD',
+  '#5EB0E5'
+],
+  rand = (min, max) => 
+    Math.floor(Math.random() * (max - min + 1)) + min,
+  selRand = (o) => o[rand(0, o.length -1)],
+  distanceTo =  (x1, y1, x2, y2) => 
+    Math.sqrt((Math.pow(x2-x1,2))+(Math.pow(y2-y1,2))),
+  shouldDraw = (x, y) => 
+    (distanceTo(x1, y1, x, y) >= dist_to_draw),
+  addStr = (x, y) => {
+    const str = document.createElement("div");
+    str.innerHTML = '&#10022;';
+    str.className = 'star';
+    str.style.top = `${y + rand(-20,20)}px`;
+    str.style.left = `${x}px`;
+    str.style.color = selRand(colors);
+    str.style.fontSize = selRand(fsize);
+    document.body.appendChild(str);
+    const fs = 10 + 5 * parseFloat(getComputedStyle(str).fontSize);
+    str.animate({
+      translate: `0 ${(y+fs)>vh?vh-y:fs}px`,
+      opacity: 0,
+      transform: `rotateX(${rand(1, 500)}deg) rotateY(${rand(1, 500)}deg)`
+    }, {
+      duration: delay,
+      fill: 'forwards',
 
-const lobbyButton = document.getElementsByClassName('modalButton')[0]; 
-const lobbyName = document.getElementById('text'); 
-const isPrivate = document.querySelector('[type=checkbox]');
-const lobbyPass = document.getElementById('passwordInput');
-
-lobbyButton.addEventListener('click', async e => {
-  try {
-    let requestBody = {
-      lobbyName: lobbyName.value,
-      isPrivate: isPrivate.checked,
-      lobbyPass: lobbyPass.value,
-      lobbyStatus: 'waiting',
-      playersNum: 1
-    };
-
-    const res = await fetch(config.host + "/lobby/createLobby", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(requestBody)
     });
-  } 
-  catch (error) {
-    console.error("Error:", error);
+    setTimeout(() => {
+        str.remove();
+      }, delay);
+  }
+
+addEventListener("mousemove", (e) => {
+  const {clientX, clientY} = e;
+  if(shouldDraw(clientX, clientY)){
+    addStr(clientX, clientY);
+    x1 = clientX;
+    y1 = clientY;
   }
 });
+
+function CancelLobby() {
+  var joinModal = document.getElementById("JoinModal");
+  joinModal.style.display = "none";
+}
+
+/*
+function CancelLobby() {
+  var joinModal = document.getElementById("JoinModal");
+  joinModal.style.display = "none";
+}function checkOverflow() {
+  var lobbies = document.querySelectorAll('.lobby');
+
+  lobbies.forEach(function(lobby) {
+    if (lobby.scrollHeight > lobby.clientHeight) {
+      lobby.style.overflowY = 'scroll';
+    } else {
+      lobby.style.overflowY = 'hidden';
+    }
+  });
+}
+
+*/
