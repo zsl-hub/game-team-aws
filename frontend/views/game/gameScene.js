0c5920440264285fa0ba4339c6cf34d6bb990316
shad0wek <forworkmillyrock@gmail.com>
2024-05-23 14:26:53 +0200

    #98-add-end-screen-view

--- frontend/views/game/gameScene.js
+++ frontend/views/game/gameScene.js
@@ -77,7 +77,6 @@ export default class GameScene extends Phaser.Scene {
                     rect.setInteractive();
                     rect.setDepth(0);
                     rect.id = data.fields[x][y].fieldId;
-
                     // Add pointerdown event to highlight the cell. Here you can add connection with database
                     rect.on('pointerdown', () => {
     
@@ -88,7 +87,6 @@ export default class GameScene extends Phaser.Scene {
                             x,
                             y
                         });
-                       
                     });
 
                     row.push(rect);
@@ -209,6 +207,10 @@ export default class GameScene extends Phaser.Scene {
         });
 
         this.continueButton.on('pointerdown', () => {
+            myChannel.publish("quitPlayer", {
+                lobbyId: lobbyId,
+                playerId: playerId,
+            })
             this.endGame(1 - this.currentPlayerIndex);
             this.modal.setVisible(false);
             this.confirmText.setVisible(false);
@@ -246,6 +248,11 @@ export default class GameScene extends Phaser.Scene {
             this.handleClick();
         });
         quitButton.setOrigin(0.5);
+
+        this.input.keyboard.on('keydown-Q', () => {
+            this.handleClick();
+        });
+
 
         this.timeText = this.add.text(width, height * 0.05, 'Time: 60', { fontSize: width * 0.05, fill: '#ffffff' });
         this.timeText.setVisible(false);
@@ -291,7 +298,6 @@ export default class GameScene extends Phaser.Scene {
     }
     //Reset the game timer and hide it from view
     resetTimer() {
-
         if (this.timer) {
             clearInterval(this.timer);
         }
@@ -344,7 +350,7 @@ export default class GameScene extends Phaser.Scene {
 
     endGame(winnerIndex) {
         if (this.timer) {
-            this.timer.remove(false);
+            clearInterval(this.timer);
         }
 
         const winner = this.players[winnerIndex];
@@ -358,10 +364,17 @@ export default class GameScene extends Phaser.Scene {
 
     endNegroGame(playerName) {
         if (this.timer) {
-            this.timer.remove(false);
+           clearInterval(this.timer);
         }
 
-        this.winnerText.setText(`${playerName} wins!`);
+        const winner = this.players[winnerIndex];
+        // Create a new graphics object for the golden background
+        const winnerBackground = this.add.graphics();
+        winnerBackground.fillStyle(0xFFD700, 1); // Gold color
+        winnerBackground.fillRect(0, 0, this.scale.width, this.scale.height);
+
+        // Create the winner's text
+        this.winnerText.setText(`${winner.name} wins!`);
         this.winnerText.setVisible(true);
 
         setTimeout(() => {
